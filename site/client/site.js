f1350a7aa433aaa8d363e0b63234ce9cac724950
Jing Jin <jing@konsu.lt>
2012-06-17 21:22:33 -0700

    Updated site to minimal showable status.

--- site/client/site.js
+++ site/client/site.js
@@ -1,8 +1,3 @@
-Template.services.serviceList = function() {
-  var services = [ {serviceName: "The Mobile Web", serviceDescription: "You have a site that looks great. Though, you have this massive surge of traffic from mobile web devices and need to adjust. We have just the solutions for you."},  
-    {serviceName: "iOS Apps", serviceDescription: "You're looking to build an iOS app or you already have one and need help to add that next killer feature. Tell us more about it."},
-    {serviceName: "Robotics", serviceDescription: "Some are into the cutting edge and others are into the future. You're looking to push the limits of the consumer robotics front and need some expertise. We've got you covered."}
-  ];
-
-  return services;
-}
+Meteor.startup (function () {
+	Modernizr.load();
+})
