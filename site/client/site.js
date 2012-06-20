3a37c69c0ef2f77105cd6534c556afe890d2e79a
Matthew Delaney <mdelaney7@gmail.com>
2012-06-20 13:16:40 -0700

    Expandable K-icon door, for funsies!

--- site/client/site.js
+++ site/client/site.js
@@ -1,3 +1,13 @@
+function draftEmail(addr)
+{
+  window.location = "mailto:"+addr;
+};
+
 Meteor.startup (function () {
 	Modernizr.load();
+
+  $("#contactus").click(function () {
+    var emailz = "us@konsu.lt";
+    draftEmail(emailz);
+  });
 })
