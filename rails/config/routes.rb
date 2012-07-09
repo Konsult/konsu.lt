ad71a699611122de6345da2e8056e05160103b44
Jing Jin <jing@konsu.lt>
2012-07-08 20:23:26 -0700

    Ported meteor site in rails.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,7 @@
 RailsSite::Application.routes.draw do
+  root :to => "home#index"
+  match "*a" => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
