0f3cdae76dada59ab8b3b01e1ab8e350b4c069e8
Jing Jin <jing@konsu.lt>
2012-07-08 19:14:23 -0700

    Created rails boiler plate.

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSite::Application.config.secret_token = '83b6113dd1dbd128bf820d395d2d6d9d42b5fceac5bcad29495f0b80ed6a3bce6ba51b2feeb3b54663511e4950fab4fe5975b3dbd6bf40859e80d32e46d0f295'
